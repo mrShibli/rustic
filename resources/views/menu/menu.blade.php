d0dde67e0a37de47a922b17c634b8f124553055c
himel <himel@gmail.com>
2024-05-11 23:31:48 +0600

    project initilized

--- resources/views/menu/menu.blade.php
+++ resources/views/menu/menu.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
 
-<h4 class="my-6 px-5 text-purple-700">Hello from Nasa</h4>
-<h4 class="my-6 text-xl px-5 text-blue-500">Hello from Nasa</h4>
-<h4 class="my-6 px-5 text-purple-700">Hello from Nasa</h4>
+<div class="w-[800px] mx-auto">
+
+
+</div>
 
 @endsection
 
