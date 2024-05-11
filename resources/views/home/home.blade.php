0f66c2c01f16df6bfe280a8c64f3606672ebfa3d
mrShibli <shibli.raihan.bangli@gmail.com>
2024-05-12 02:29:32 +0600

    hero

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,8 +2,38 @@
 
 @section('content')
 
-
-<h1>Home</h1>
+<section class="md:h-[100vh] h-[50vh] w-full bg-cover bg-center" style="background-image: url({{ asset('img/home.webp') }})">
+    <header class="grid grid-cols-1 divide-y pt-3 divide-[#bc9b75] text-white w-[98%] mx-auto">
+        <div class="mb-1 flex justify-between font-light md:text-[12px] text-[9px]">
+            <span>Wed-Sun: 9:30a-8p</span>
+            <span>+61 3 5208 1317</span>
+        </div>
+        <div></div>
+      </header>
+      <nav class="w-[90%] mx-auto">
+        <div class="flex justify-between content-center ">
+            <div>
+                <img class="w-[70px] md:w-[100px]" src="{{ asset('img/Rustic Ram.png') }}" alt="" srcset="">
+            </div>
+            <div class="flex flex-row content-center md:gap-5 gap-3">
+                <div>
+                    <button class="text-white md:text-[15px] text-[13px] mt-5">Menu</button>
+                </div>
+                <div>
+                    <button class="text-white md:text-[15px] text-[13px] mt-5">Booking</button>
+                </div>
+                <div>
+                    <button class="text-white md:text-[15px] text-[13px] mt-5">Contact</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <main class="text-white w-[40%] mx-auto">
+        <h1 class="hero-font md:text-[50px] sm:text-[50px] text-[40px]">-Dear</h1>
+        <p class="hero-font md:text-[90px] sm:text-[50px] text-[20px] -mt-10">Welcome to</p>
+        <h1 class="hero-font md:text-[100px]  sm:text-[50px] text-[40px] -mt-14 md:ml-20 font-bold text-[#ffb766]">Rusticram</h1>
+    </main>
+</section>
 @endsection
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('header')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
+    <style>
+        body {
+            font-family: "Jost", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: <weight>;
+            font-style: normal;
+        }
+        .hero-font{
+            font-family: "Cormorant", serif;
+            font-optical-sizing: auto;
+            font-weight: <weight>;
+            font-style: normal;
+
+        }
+    </style>
 </head>
 
 <body>
